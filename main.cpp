eec8d92f5b39aa8b586e95ead2407320483fe6cc
ElizaMaka <elizamaka95@gmail.com>
2024-12-08 09:27:16 +0545

    wip

--- main.cpp
+++ main.cpp
@@ -12,11 +12,46 @@ int tasksCount = 0;
 const string File_Name = "tasks.txt";
 
 void addTasks();
-void deleteTasks();
+void deleteTasks()
+{
+    view();
+    if (tasksCount == 0)
+        return;
+    cout << "Enter the task number to delete: ";
+    int taskNumber;
+    cin >> taskNumber;
+    if (taskNumber < 1 || taskNumber > tasksCount)
+    {
+        cout << "Invalid task number!" << endl;
+    }
+    for (int i = taskNumber - 1; i < tasksCount - 1; i++)
+    {
+        Tasks[i] = Tasks[i + 1];
+        Condition[i] = Condition[i + 1];
+    }
+    tasksCount--;
+    cout << "Task deleted successfully!" << endl;
+};
+
 void loadTasks();
 void saveTasks();
 void view();
-void markComplete();
+void markComplete()
+{
+    view();
+    if (tasksCount == 0)
+        return;
+    cout << "Enter the task number to mark as completed: ";
+    int taskNumber;
+    cin >> taskNumber;
+    if (taskNumber < 1 || taskNumber > tasksCount)
+    {
+        cout << "Invalid task number!" << endl;
+        return;
+    }
+    Condition[taskNumber - 1] = true;
+    cout << "Task marked as completed!" << endl;
+};
 
 int main()
 {
