089132d679095692f893a16a3bca17b08797d585
Nishtha413 <nishthamhjrn@gmail.com>
2024-12-08 20:20:42 +0545

    merge main and complete

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int tasksCount = 0;
 const string File_Name = "tasks.txt";
 fstream taskfile;
 
-void addTasks();
 void deleteTasks()
 {
     view();
@@ -34,9 +33,6 @@ void deleteTasks()
     cout << "Task deleted successfully!" << endl;
 };
 
-void loadTasks();
-void saveTasks();
-void view();
 void markComplete()
 {
     view();
@@ -92,42 +88,42 @@ void loadTasks()
 }
 int main()
 {
-   loadTasks(); //load previous tasks
+    loadTasks(); // load previous tasks
 
     int choice;
-    do {
-    cout <<"\n======= To-Do list Menu=======" <<endl;
-    cout <<"1. Add a New task"<< endl;
-    cout << "2. View All Tasks" << endl;
-    cout << "3. Mark Task as Completed" << endl;
-    cout << "4. Delete a Task" << endl;
-    cout << "5. Save and Exit" << endl;
-    cout << "=====================================" << endl;
-    cout << "Please select an option (1-5): ";
-    cin >> choice;
+    do
+    {
+        cout << "\n======= To-Do list Menu=======" << endl;
+        cout << "1. Add a New task" << endl;
+        cout << "2. View All Tasks" << endl;
+        cout << "3. Mark Task as Completed" << endl;
+        cout << "4. Delete a Task" << endl;
+        cout << "5. Save and Exit" << endl;
+        cout << "=====================================" << endl;
+        cout << "Please select an option (1-5): ";
+        cin >> choice;
 
-
-   switch (choice) {
-            case 1:
-                addTasks(); 
-                break;
-            case 2:
-                view();
-                break;
-            case 3:
-                markComplete();
-                break;
-            case 4:
-                deleteTasks();
-                break;
-            case 5:
-                saveTasks();
-                break;
-            default:
-                cout << "Invalid choice! Please try again." << endl;
+        switch (choice)
+        {
+        case 1:
+            addTasks();
+            break;
+        case 2:
+            view();
+            break;
+        case 3:
+            markComplete();
+            break;
+        case 4:
+            deleteTasks();
+            break;
+        case 5:
+            saveTasks();
+            break;
+        default:
+            cout << "Invalid choice! Please try again." << endl;
         }
-    } while (choice>=1 && choice<=5 );
+    } while (choice >= 1 && choice <= 5);
 
     return 0;
 }
-
